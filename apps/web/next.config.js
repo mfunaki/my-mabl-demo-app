680a93fe1ef2847b0ea79b01d6b00fc9fd9345d4
Masahiko Funaki <mfunaki@gmail.com>
2025-12-07 00:35:01 +0900

    Cloud Runデプロイメント用のGitHub Actionsワークフローを新規作成し、Dockerfileと設定を更新

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  experimental: {
-    outputFileTracingIncludes: {
-      '/': ['./public/**/*'],
-    },
-  },
 }
 
 module.exports = nextConfig
